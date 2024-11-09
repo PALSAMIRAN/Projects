acfa59c95b54b3438ca28e244804bc04e730aa70
Samiran Pal <115138802+PALSAMIRAN@users.noreply.github.com>
2024-11-09 18:15:59 +0530

    Add files via upload

---
+++ clock/index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`; // Corrected variable name
+    second.style.transform = `rotate(${srotation}deg)`; // Corrected variable name
+}, 1000);
